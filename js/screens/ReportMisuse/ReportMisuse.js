4474ad5c5e383b65cacb47e637f3644dcb9f0f57
Andrei Egorov <andreyegorov1992@gmail.com>
2018-09-22 13:20:52 -0700

    Add image picker

--- js/screens/ReportMisuse/ReportMisuse.js
+++ js/screens/ReportMisuse/ReportMisuse.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
+import TakePicture from '../../components/TakePicture'
 const ReportMisuse = () => {
   return (
     <View>
       <Text> I am A Report Misuse page !!! </Text>
+      <TakePicture/>
     </View>
   );
 };
