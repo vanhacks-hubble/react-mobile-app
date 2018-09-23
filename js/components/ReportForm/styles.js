208850f4ccb0fbaa119f21f84e7a323e9dd5ae0f
Andrei Egorov <andreyegorov1992@gmail.com>
2018-09-22 18:57:05 -0700

    Add styles

--- js/components/ReportForm/styles.js
+++ js/components/ReportForm/styles.js
@@ -1,10 +1,14 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 import { colors } from '../../config/styles';
+const { height, width } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   root: {
     // flexGrow: 1,
     backgroundColor: "white",
+    height: height*0.6,
+    marginTop: height*0.4
+    
     
 
     // justifyContent: 'flex-end',
