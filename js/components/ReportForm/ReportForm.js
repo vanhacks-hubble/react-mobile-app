bdb7134e1124163931c3508122803849dc7a9c10
Andrei Egorov <andreyegorov1992@gmail.com>
2018-09-22 21:25:41 -0700

    Style Report screen

--- js/components/ReportForm/ReportForm.js
+++ js/components/ReportForm/ReportForm.js
@@ -1,38 +1,43 @@
 import React from 'react';
-import { TextInput, View, Text } from 'react-native';
+import { TextInput, View, Text, TouchableOpacity } from 'react-native';
 import { Button } from 'react-native-elements';
 import styles from './styles';
 const ReportForm = ({ handleInputText, resetForm, submitReport }) => {
   return (
     <View style={styles.root}>
-      <View>
-        <Text styles={styles.inputTitle}>
-          {' '}
-          What is the Validated Busioness Name?
-        </Text>
-        <TextInput
-          style={styles.businessName}
-          placeholder="Business Name"
-          onChangeText={text => {
-            handleInputText('BusinessName', text);
-          }}
-        />
+      <View style={styles.textInputContainer}>
+        <View>
+          <Text style={styles.inputTitle}>
+            What is the Validated Business Name?
+          </Text>
+          <TextInput
+            style={styles.businessName}
+            placeholder="Business Name"
+            onChangeText={text => {
+              handleInputText('BusinessName', text);
+            }}
+          />
+        </View>
+        <View>
+          <Text style={styles.inputTitle}>Please Describe the Misuse</Text>
+          <TextInput
+            style={styles.comment}
+            placeholder="Comments"
+            multiline={true}
+            numberOfLines={6}
+            onChangeText={text => {
+              handleInputText('Comment', text);
+            }}
+          />
+        </View>
       </View>
-      <View>
-        <Text styles={styles.inputTitle}>Please Describe the Misuse</Text>
-        <TextInput
-          style={styles.comment}
-          placeholder="Comments"
-          multiline={true}
-          numberOfLines={6}
-          onChangeText={text => {
-            handleInputText('Comment', text);
-          }}
-        />
-      </View>
-      <View>
-        <Button title="Reset" onPress={() => resetForm()} />
-        <Button title="Submit" onPress={() => submitReport()} />
+      <View style={styles.buttonWrapper}>
+        <TouchableOpacity onPress={() => resetForm()} style={styles.button}>
+          <Text style={styles.buttonText}>RESET</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => submitReport()} style={styles.button}>
+          <Text style={styles.buttonText}>SUBMIT</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
